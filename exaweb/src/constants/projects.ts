2bebaa0238459e7c0ed992210d1e84424e89bc60
Gabriel Lobo <globo.coding@gmail.com>
2024-03-23 21:32:45 -0600

    Paso #5 Completado

---
+++ exaweb/src/constants/projects.ts
@@ -0,0 +1,436 @@
+
+const cloudinaryBase = 'https://res.cloudinary.com/glovooker/image/upload/v1702798842/portfolio/';
+
+export const engage360Cover = `${ cloudinaryBase }/projects/engage360.jpg`;
+
+export const easylistCover = `${ cloudinaryBase }/projects/easylist.jpg`;
+export const easylist1 = `${ cloudinaryBase }/projects/easylist/1.jpeg`;
+export const easylist2 = `${ cloudinaryBase }/projects/easylist/2.jpeg`;
+export const easylist3 = `${ cloudinaryBase }/projects/easylist/3.png`;
+
+export const cenfotecoKingdomCover = `${ cloudinaryBase }/projects/cenfoteco-kingdoms.jpg`;
+export const cenfotecoKingdom1 = `${ cloudinaryBase }/projects/cenfoteco-kingdoms/1.png`;
+export const cenfotecoKingdom2 = `${ cloudinaryBase }/projects/cenfoteco-kingdoms/2.png`;
+export const cenfotecoKingdom3 = `${ cloudinaryBase }/projects/cenfoteco-kingdoms/3.png`;
+
+export const elViejoCover = `${ cloudinaryBase }/projects/el-viejo.jpg`;
+
+export const mawiCover = `${ cloudinaryBase }/projects/mawi.jpg`;
+export const mawi1 = `${ cloudinaryBase }/projects/mawi/1.png`;
+export const mawi2 = `${ cloudinaryBase }/projects/mawi/2.png`;
+export const mawi3 = `${ cloudinaryBase }/projects/mawi/3.png`;
+
+export const stariesCover = `${ cloudinaryBase }/projects/staries.jpg`;
+export const staries1 = `${ cloudinaryBase }/projects/staries/1.png`;
+export const staries2 = `${ cloudinaryBase }/projects/staries/2.png`;
+export const staries3 = `${ cloudinaryBase }/projects/staries/3.png`;
+
+export const mobydygCover = `${ cloudinaryBase }/projects/mobydyg-site.jpg`;
+export const mobydygSite1 = `${ cloudinaryBase }/projects/mobydyg-site/1.png`;
+export const mobydygSite2 = `${ cloudinaryBase }/projects/mobydyg-site/2.png`;
+export const mobydygSite3 = `${ cloudinaryBase }/projects/mobydyg-site/3.png`;
+
+export const mobydygClientCore1 = `${ cloudinaryBase }/projects/mobydyg-client-core/1.png`;
+export const mobydygClientCore2 = `${ cloudinaryBase }/projects/mobydyg-client-core/2.png`;
+export const mobydygClientCore3 = `${ cloudinaryBase }/projects/mobydyg-client-core/3.png`;
+
+export const mobydygBackoffice1 = `${ cloudinaryBase }/projects/mobydyg-backoffice/1.png`;
+export const mobydygBackoffice2 = `${ cloudinaryBase }/projects/mobydyg-backoffice/2.png`;
+export const mobydygBackoffice3 = `${ cloudinaryBase }/projects/mobydyg-backoffice/3.png`;
+
+export const tournamentCover = `${ cloudinaryBase }/projects/tournament.jpg`;
+export const tournament1 = `${ cloudinaryBase }/projects/fct-tournament/1.png`;
+export const tournament2 = `${ cloudinaryBase }/projects/fct-tournament/2.png`;
+export const tournament3 = `${ cloudinaryBase }/projects/fct-tournament/3.png`;
+
+export const mokaCover = `${ cloudinaryBase }/projects/moka.jpg`;
+export const moka1 = `${ cloudinaryBase }/projects/moka/1.png`;
+export const moka2 = `${ cloudinaryBase }/projects/moka/2.png`;
+export const moka3 = `${ cloudinaryBase }/projects/moka/3.png`;
+
+export const tummyCover = `${ cloudinaryBase }/projects/tummy.jpg`;
+export const tummy1 = `${ cloudinaryBase }/projects/tummy/1.png`;
+export const tummy2 = `${ cloudinaryBase }/projects/tummy/2.png`;
+export const tummy3 = `${ cloudinaryBase }/projects/tummy/3.png`;
+
+export const cleverSolutionsCover = `${ cloudinaryBase }/projects/clever-solutions.jpg`;
+export const cleverSolutions1 = `${ cloudinaryBase }/projects/clever-solutions/1.png`;
+export const cleverSolutions2 = `${ cloudinaryBase }/projects/clever-solutions/2.png`;
+export const cleverSolutions3 = `${ cloudinaryBase }/projects/clever-solutions/3.png`;
+
+export const personalBrandCover = `${ cloudinaryBase }/projects/content-brand-book.jpg`;
+export const personalBrand1 = `${ cloudinaryBase }/projects/personal-brand/1.png`;
+export const personalBrand2 = `${ cloudinaryBase }/projects/personal-brand/2.png`;
+export const personalBrand3 = `${ cloudinaryBase }/projects/personal-brand/3.png`;
+export const personalBrand4 = `${ cloudinaryBase }/projects/personal-brand/4.png`;
+export const personalBrand5 = `${ cloudinaryBase }/projects/personal-brand/5.png`;
+export const personalBrand6 = `${ cloudinaryBase }/projects/personal-brand/6.png`;
+export const personalBrand7 = `${ cloudinaryBase }/projects/personal-brand/7.png`;
+export const personalBrand8 = `${ cloudinaryBase }/projects/personal-brand/8.png`;
+export const personalBrand9 = `${ cloudinaryBase }/projects/personal-brand/9.png`;
+export const personalBrand10 = `${ cloudinaryBase }/projects/personal-brand/10.png`;
+export const personalBrand11 = `${ cloudinaryBase }/projects/personal-brand/11.png`;
+export const personalBrand12 = `${ cloudinaryBase }/projects/personal-brand/12.png`;
+export const personalBrand13 = `${ cloudinaryBase }/projects/personal-brand/13.png`;
+export const personalBrand14 = `${ cloudinaryBase }/projects/personal-brand/14.png`;
+export const personalBrand15 = `${ cloudinaryBase }/projects/personal-brand/15.png`;
+
+export const ikiCover = `${ cloudinaryBase }/projects/iki.jpg`;
+export const iki1 = `${ cloudinaryBase }/projects/iki/1.png`;
+export const iki2 = `${ cloudinaryBase }/projects/iki/2.png`;
+export const iki3 = `${ cloudinaryBase }/projects/iki/3.png`;
+export const iki4 = `${ cloudinaryBase }/projects/iki/4.png`;
+export const iki5 = `${ cloudinaryBase }/projects/iki/5.png`;
+
+export const findingTheMeaningCover = `${ cloudinaryBase }/projects/finding-the-meaning.jpg`;
+export const findingTheMeaning1 = `${ cloudinaryBase }/projects/finding-the-meaning/1.png`;
+export const findingTheMeaning2 = `${ cloudinaryBase }/projects/finding-the-meaning/2.png`;
+export const findingTheMeaning3 = `${ cloudinaryBase }/projects/finding-the-meaning/3.png`;
+export const findingTheMeaning4 = `${ cloudinaryBase }/projects/finding-the-meaning/4.png`;
+export const findingTheMeaning5 = `${ cloudinaryBase }/projects/finding-the-meaning/5.png`;
+export const findingTheMeaning6 = `${ cloudinaryBase }/projects/finding-the-meaning/6.png`;
+export const findingTheMeaning7 = `${ cloudinaryBase }/projects/finding-the-meaning/7.png`;
+export const findingTheMeaning8 = `${ cloudinaryBase }/projects/finding-the-meaning/8.png`;
+export const findingTheMeaning9 = `${ cloudinaryBase }/projects/finding-the-meaning/9.png`;
+export const findingTheMeaning10 = `${ cloudinaryBase }/projects/finding-the-meaning/10.png`;
+
+import { tags } from './tags';
+import { TagProps } from '../components/ProjectCard/ProjectCard';
+import { Avatar } from '../components/Avatars/Avatars';
+import { contributors } from './contributors';
+
+type ProjectList = {
+    tags: TagProps[];
+    title: string;
+    categ: string;
+    coverImg: string;
+    desc: string;
+    longDesc: string;
+    textColor: string;
+    tagsBgColor: string;
+    projectImg?: string[];
+    contributors: Avatar[];
+    previewLink?: string;
+    githubLink?: string;
+    figmaLink?: string;
+};
+
+export const PROJECTS: ProjectList[] = [
+    {
+        tags: [tags.react, tags.typescript, tags.materialui, tags.graphql, tags.apollo, tags.dgraph],
+        title: 'engage360.title',
+        categ: 'SOFT · UI/UX',
+        coverImg: engage360Cover,
+        desc: 'engage360.description',
+        longDesc: 'engage360.long_description',
+        textColor: 'text-[#0574b9]',
+        tagsBgColor: 'bg-[#0574b9]',
+        contributors: [
+            contributors.glovooker,
+            contributors.woodsoul,
+            contributors.marceloAlbacete,
+        ],
+        previewLink: 'https://www.ivacus.com/soluciones',
+    },
+    {
+        tags: [tags.html, tags.css, tags.javascript, tags.jquery, tags.bootstrap, tags.csharp, tags.dotnet, tags.sqlserver, tags.azuredevops],
+        title: 'easylist.title',
+        categ: 'SOFT · UI/UX · DES',
+        coverImg: easylistCover,
+        desc: 'easylist.description',
+        longDesc: 'easylist.long_description',
+        textColor: 'text-[#80AAA0]',
+        tagsBgColor: 'bg-[#80AAA0]',
+        projectImg: [easylist1, easylist2, easylist3],
+        contributors: [
+            contributors.glovooker,
+            contributors.forlucho,
+            contributors.bistec,
+            contributors.keylorGomez,
+            contributors.isaacSandoval,
+        ],
+        githubLink: 'https://github.com/glovooker/easylist',
+    },
+    {
+        tags: [tags.java],
+        title: 'cenfotecoKingdoms.title',
+        categ: 'SOFT · DES',
+        coverImg: cenfotecoKingdomCover,
+        desc: 'cenfotecoKingdoms.description',
+        longDesc: 'cenfotecoKingdoms.long_description',
+        textColor: 'text-[#816077]',
+        tagsBgColor: 'bg-[#816077]',
+        projectImg: [cenfotecoKingdom1, cenfotecoKingdom2, cenfotecoKingdom3],
+        contributors: [
+            contributors.glovooker,
+            contributors.woodsoul,
+            contributors.saturnxs,
+            contributors.bistec,
+            contributors.jocselynAguilar,
+            contributors.marcosSaenz,
+            contributors.sebastianLopez,
+        ],
+        githubLink: 'https://github.com/glovooker/cenfoteco-kingdoms',
+    },
+    {
+        tags: [tags.mathematica, tags.react, tags.typescript, tags.redux, tags.materialui],
+        title: 'elViejo.title',
+        categ: 'RESEARCH · DATA · SOFT',
+        coverImg: elViejoCover,
+        desc: 'elViejo.description',
+        longDesc: 'elViejo.long_description',
+        textColor: 'text-[#85c651]',
+        tagsBgColor: 'bg-[#85c651]',
+        contributors: [
+            contributors.glovooker,
+            contributors.woodsoul,
+            contributors.tomasDeCamino,
+            contributors.luisNaranjo,
+        ],
+        previewLink: 'https://www.azucareraelviejo.com/es/noticias/cenfotec-y-azucarera-el-viejo-se-alian-en-programa-de-investigacion-para-impulsar',
+    },
+    {
+        tags: [tags.react, tags.typescript, tags.tailwind, tags.redux, tags.figma],
+        title: 'mawi.title',
+        categ: 'SOFT · UI/UX',
+        coverImg: mawiCover,
+        desc: 'mawi.description',
+        longDesc: 'mawi.long_description',
+        textColor: 'text-[#36015d]',
+        tagsBgColor: 'bg-[#36015d]',
+        projectImg: [mawi1, mawi2, mawi3],
+        contributors: [
+            contributors.glovooker,
+            contributors.sergioMonge,
+            contributors.arielMontero,
+        ],
+        previewLink: 'https://managers.mawi.io/'
+    },
+    {
+        tags: [tags.react, tags.typescript, tags.nodejs, tags.firebase, tags.figma],
+        title: 'staries.title',
+        categ: 'SOFT · UI/UX · DES',
+        coverImg: stariesCover,
+        desc: 'staries.description',
+        longDesc: 'staries.long_description',
+        previewLink: 'https://staries-skemclub.web.app/',
+        githubLink: 'https://github.com/glovooker/staries',
+        textColor: 'text-[#3f8293]',
+        tagsBgColor: 'bg-[#3f8293]',
+        projectImg: [staries1, staries2, staries3],
+        contributors: [
+            contributors.glovooker,
+            contributors.woodsoul,
+            contributors.saturnxs,
+            contributors.anderlyn,
+            contributors.fabianMena,
+        ],
+    },
+    {
+        tags: [tags.angular, tags.scss, tags.typescript, tags.aws, tags.figma],
+        title: 'mobydygSite.title',
+        categ: 'SOFT · UI/UX · DES',
+        coverImg: mobydygCover,
+        desc: 'mobydygSite.description',
+        longDesc: 'mobydygSite.long_description',
+        previewLink: 'https://mobydyg.com/',
+        textColor: 'text-[#EA803F]',
+        tagsBgColor: 'bg-[#EA803F]',
+        projectImg: [mobydygSite1, mobydygSite2, mobydygSite3],
+        contributors: [contributors.glovooker],
+    },
+    {
+        tags: [tags.angular, tags.scss, tags.typescript, tags.aws, tags.mysql],
+        title: 'mobydygCore.title',
+        categ: 'SOFT · DES',
+        coverImg: mobydygCover,
+        desc: 'mobydygCore.description',
+        longDesc: 'mobydygCore.long_description',
+        previewLink: 'https://merakienlinea.com/',
+        textColor: 'text-[#EA803F]',
+        tagsBgColor: 'bg-[#EA803F]',
+        projectImg: [mobydygClientCore1, mobydygClientCore2, mobydygClientCore3],
+        contributors: [
+            contributors.glovooker,
+            contributors.woodsoul,
+            contributors.saturnxs,
+            contributors.eduardoFont,
+        ],
+    },
+    {
+        tags: [
+            tags.angular,
+            tags.scss,
+            tags.typescript,
+            tags.aws,
+            tags.mysql,
+            tags.figma,
+        ],
+        title: 'mobydygBackoffice.title',
+        categ: 'SOFT · UI/UX · DES',
+        coverImg: mobydygCover,
+        desc: 'mobydygBackoffice.description',
+        longDesc: 'mobydygBackoffice.long_description',
+        previewLink: 'https://portal.mobydyg.com/',
+        textColor: 'text-[#EA803F]',
+        tagsBgColor: 'bg-[#EA803F]',
+        projectImg: [mobydygBackoffice1, mobydygBackoffice2, mobydygBackoffice3],
+        contributors: [
+            contributors.glovooker,
+            contributors.woodsoul,
+            contributors.saturnxs,
+            contributors.eduardoFont,
+        ],
+    },
+    {
+        tags: [tags.react, tags.dotnet, tags.csharp, tags.sqlserver, tags.figma],
+        title: 'tournament.title',
+        categ: 'SOFT · UI/UX · DES',
+        coverImg: tournamentCover,
+        desc: 'tournament.description',
+        longDesc: 'tournament.long_description',
+        textColor: 'text-[#f2bc1c]',
+        tagsBgColor: 'bg-[#f2bc1c]',
+        projectImg: [tournament1, tournament2, tournament3],
+        contributors: [
+            contributors.glovooker,
+            contributors.woodsoul,
+            contributors.saturnxs,
+            contributors.estefaniaRegidor,
+        ],
+    },
+    {
+        tags: [
+            tags.html,
+            tags.css,
+            tags.javascript,
+            tags.nodejs,
+            tags.mongodb,
+            tags.figma,
+        ],
+        title: 'moka.title',
+        categ: 'SOFT · UI/UX · DES',
+        coverImg: mokaCover,
+        desc: 'moka.description',
+        longDesc: 'moka.long_description',
+        githubLink: 'https://github.com/glovooker/veterinaria-moka',
+        textColor: 'text-[#82b29a]',
+        tagsBgColor: 'bg-[#82b29a]',
+        projectImg: [moka1, moka2, moka3],
+        contributors: [
+            contributors.glovooker,
+            contributors.anabelleVelasquez,
+            contributors.jeremyVillegas,
+            contributors.alvaroCastillo,
+            contributors.andreyVillalobos,
+            contributors.brandonLen,
+        ],
+    },
+    {
+        tags: [tags.html, tags.css, tags.javascript, tags.php],
+        title: 'tummy.title',
+        categ: 'SOFT · DES',
+        coverImg: tummyCover,
+        desc: 'tummy.description',
+        longDesc: 'tummy.long_description',
+        githubLink: 'https://github.com/glovooker/tummy',
+        textColor: 'text-[#fe5470]',
+        tagsBgColor: 'bg-[#fe5470]',
+        projectImg: [tummy1, tummy2, tummy3],
+        contributors: [
+            contributors.glovooker,
+            contributors.monseSandi,
+            contributors.luciaManzanares,
+        ],
+    },
+    {
+        tags: [tags.html, tags.css, tags.javascript, tags.figma],
+        title: 'cleverSolutions.title',
+        categ: 'SOFT · UI/UX · DES',
+        coverImg: cleverSolutionsCover,
+        desc: 'cleverSolutions.description',
+        longDesc: 'cleverSolutions.long_description',
+        previewLink: 'https://glovooker.github.io/landing-page-bisoft-1/',
+        githubLink: 'https://github.com/glovooker/landing-page-bisoft-1',
+        textColor: 'text-[#492e71]',
+        tagsBgColor: 'bg-[#492e71]',
+        projectImg: [cleverSolutions1, cleverSolutions2, cleverSolutions3],
+        contributors: [
+            contributors.glovooker,
+            contributors.anabelleVelasquez,
+            contributors.jeremyVillegas,
+            contributors.alvaroCastillo,
+            contributors.andreyVillalobos,
+            contributors.brandonLen,
+        ],
+    },
+    {
+        tags: [tags.figma],
+        title: 'personalBrand.title',
+        categ: 'DES',
+        coverImg: personalBrandCover,
+        desc: 'personalBrand.description',
+        longDesc: 'personalBrand.long_description',
+        figmaLink:
+            'https://www.figma.com/file/M85LUPS1P2iWSpW9n90kdZ/Just-Another-Personal-Design-Style?node-id=1%3A42&t=H74Gb6JpkYdjb7Ij-1',
+        textColor: 'text-[#4b7877]',
+        tagsBgColor: 'bg-[#4b7877]',
+        projectImg: [
+            personalBrand1,
+            personalBrand2,
+            personalBrand3,
+            personalBrand4,
+            personalBrand5,
+            personalBrand6,
+            personalBrand7,
+            personalBrand8,
+            personalBrand9,
+            personalBrand10,
+            personalBrand11,
+            personalBrand12,
+            personalBrand13,
+            personalBrand14,
+            personalBrand15,
+        ],
+        contributors: [contributors.glovooker],
+    },
+    {
+        tags: [tags.figma],
+        title: 'iki.title',
+        categ: 'UI/UX · DES',
+        coverImg: ikiCover,
+        desc: 'iki.description',
+        longDesc: 'iki.long_description',
+        figmaLink:
+            'https://www.figma.com/file/iJu9bgjogKUKHkz0g2huKq/CEDES---iki-Imagen-Gr%C3%A1fica?node-id=0%3A1&t=pu70NTQ9MEECXKA0-1',
+        textColor: 'text-[#4699c1]',
+        tagsBgColor: 'bg-[#4699c1]',
+        projectImg: [iki1, iki2, iki3, iki4, iki5],
+        contributors: [contributors.glovooker],
+    },
+    {
+        tags: [tags.figma],
+        title: 'findingTheMeaning.title',
+        categ: 'DES',
+        coverImg: findingTheMeaningCover,
+        desc: 'findingTheMeaning.description',
+        longDesc: 'findingTheMeaning.long_description',
+        figmaLink:
+            'https://www.figma.com/file/VtbRxXh3YR59WZ5vohYcTz/Cenfotec---Finding-The-Meaning-Content-Book?node-id=251%3A2&t=jz3Fxonbwy3DsS38-1',
+        textColor: 'text-[#0094ff]',
+        tagsBgColor: 'bg-[#0094ff]',
+        projectImg: [
+            findingTheMeaning1,
+            findingTheMeaning2,
+            findingTheMeaning3,
+            findingTheMeaning4,
+            findingTheMeaning5,
+            findingTheMeaning6,
+            findingTheMeaning7,
+            findingTheMeaning8,
+            findingTheMeaning9,
+            findingTheMeaning10,
+        ],
+        contributors: [contributors.glovooker, contributors.woodsoul],
+    },
+];
---
+++ exaweb/src/pages/Home.tsx
@@ -0,0 +1,119 @@
+import { useEffect, useRef, useState } from 'react';
+
+import { FiArrowUpCircle } from "react-icons/fi";
+import { withBasicLayout } from '../layout/basicLayout';
+import AboutMe from '../sections/AboutMe';
+import Hero from '../sections/Hero';
+import Portfolio from '../sections/Portfolio';
+import Carousel from '../sections/Carousel';
+import Contact from '../sections/Contact';
+import { Button } from '../components';
+
+export type SectionsReferences = {
+    [key: string]: {
+        ref: React.RefObject<HTMLElement>;
+        scrollTo: () => void;
+    };
+};
+
+export const Home = () => {
+    const [showButton, setShowButton] = useState(false);
+
+
+    const heroRef = useRef<HTMLElement>(null);
+    const aboutMeRef = useRef<HTMLElement>(null);
+    const portfolioRef = useRef<HTMLElement>(null);
+    const contactRef = useRef<HTMLElement>(null);
+
+    const headerHeight = 96;
+
+    const sectionsRef: SectionsReferences = {
+        heroRef: {
+            ref: heroRef,
+            scrollTo: () => {
+                const offset = (heroRef.current?.getBoundingClientRect().top ?? 0) - headerHeight;
+                window.scrollTo({
+                    top: offset,
+                    behavior: 'smooth',
+                });
+            },
+        },
+        aboutMeRef: {
+            ref: aboutMeRef,
+            scrollTo: () => {
+                const offset = (aboutMeRef.current?.getBoundingClientRect().top ?? 0) - headerHeight;
+                window.scrollTo({
+                    top: offset,
+                    behavior: 'smooth',
+                });
+            },
+        },
+        portfolioRef: {
+            ref: portfolioRef,
+            scrollTo: () => {
+                const offset = (portfolioRef.current?.getBoundingClientRect().top ?? 0) - headerHeight;
+                window.scrollTo({
+                    top: offset,
+                    behavior: 'smooth',
+                });
+            },
+        },
+        contactRef: {
+            ref: contactRef,
+            scrollTo: () => {
+                const offset = (contactRef.current?.getBoundingClientRect().top ?? 0) - headerHeight;
+                window.scrollTo({
+                    top: offset,
+                    behavior: 'smooth',
+                });
+            },
+        },
+    };
+
+    useEffect(() => {
+        const checkScrollPosition = () => {
+            if (window.scrollY > 90) {
+                setShowButton(true);
+            } else {
+                setShowButton(false);
+            }
+        };
+
+        window.addEventListener('scroll', checkScrollPosition);
+
+        // Clean up the event listener when the component is unmounted
+        return () => {
+            window.removeEventListener('scroll', checkScrollPosition);
+        };
+    }, []);
+
+    return (
+        <div>
+            <Hero
+                reference={ heroRef }
+                sectionsRef={ sectionsRef }
+            />
+            <Carousel />
+            <AboutMe
+                title={ 'title' }
+                description={ ['description_1', 'description_2'] }
+                reference={ aboutMeRef }
+                sectionsRef={ sectionsRef }
+                isLeft={ true }
+            />
+            <Portfolio reference={ portfolioRef } />
+            <Contact reference={ contactRef } sectionsRef={ sectionsRef } />
+
+            { showButton && (
+                <Button
+                    isFloating={ true }
+                    bgColor="bg-glovooker-chamoisee-100"
+                    icon={ <FiArrowUpCircle className="h-8 w-8" /> }
+                    onClick={ () => window.scrollTo({ top: 0, behavior: 'smooth' }) }
+                />
+            ) }
+        </div>
+    );
+};
+
+export default withBasicLayout(Home);
